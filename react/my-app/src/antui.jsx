44079c8a8692746fe3ba972aca2d8f77b27625f7
kanonair <15954027738@163.com>
2019-10-23 17:15:19 +0800

    init

---
+++ react/my-app/src/antui.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Button} from 'antd-mobile'
+
+export default class Antui extends React.Component {
+    render() {
+        return (
+            <Button>按钮</Button>
+        )
+    }
+}
--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -4,7 +4,9 @@ import {BrowserRouter} from 'react-router-dom'
 import CommentComponent from './comment'
 import AjaxComponent from './ajax'
 import Spa from './spa'
+import Antui from './antui'
 
 ReactDOM.render(<CommentComponent/>, document.getElementById('root01'))
 ReactDOM.render(<AjaxComponent/>, document.getElementById('root02'))
 ReactDOM.render(<BrowserRouter><Spa/></BrowserRouter>, document.getElementById('root03'))
+ReactDOM.render(<Antui/>, document.getElementById('root04'))
