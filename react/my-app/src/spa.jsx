9e2b13cab3c3aa4877684eac3c8c5674efd6ee3f
kanonair <15954027738@163.com>
2019-10-23 16:50:09 +0800

    init

--- react/my-app/src/spa.jsx
+++ react/my-app/src/spa.jsx
@@ -32,6 +32,63 @@ class Home extends React.Component {
         return (
             <div>
                 Home
+                <NavLink to='/home/news'>News</NavLink>
+                <NavLink to='/home/messages'>Messages</NavLink>
+                <Switch>
+                    <Route component={News} path='/home/news'/>
+                    <Route component={Messages} path='/home/messages'/>
+                    <Redirect to='/home/news'/>
+                </Switch>
+            </div>
+        )
+    }
+}
+
+class News extends React.Component {
+    render() {
+        return (
+            <div>
+                News
+            </div>
+        )
+    }
+}
+
+class Messages extends React.Component {
+
+    state = {
+        messages: [
+            {id: 1, title: 'message01'},
+            {id: 2, title: 'message02'}
+        ]
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.messages.map(message =>
+                        (<li key={message.id}>
+                            <NavLink to={`/home/messages/detail/${message.id}`}>{message.title}</NavLink>
+                            <button
+                                onClick={() => this.props.history.push(`/home/messages/detail/${message.id}`)}>push
+                            </button>
+                            <button
+                                onClick={() => this.props.history.replace(`/home/messages/detail/${message.id}`)}>replace
+                            </button>
+                        </li>))}
+                </ul>
+                <Route component={Detail} path='/home/messages/detail/:id'/>
+            </div>
+        )
+    }
+}
+
+class Detail extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.match.params.id}
             </div>
         )
     }
